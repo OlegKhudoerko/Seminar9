0e52c9cb3ebf0e9fe8ddecc6f8d4898d3926d938
OlegKhudoerko <292oleg292@mail.ru>
2022-09-06 22:17:57 +0300

    sum vector

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -8,12 +8,17 @@ Console.Write("\nВведите значение числа M = ");
 int M = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите значение числа N = ");
 int N = Convert.ToInt32(Console.ReadLine());
-
+int temp = 0;
 int SumNumbers(int start, int end)
 {
-       if (M > N) return -1; //error input
-       if (start == end) return start;
-       return (start + SumNumbers(start + 1, end));
+    if (start > end) //При М>N меняем направление суммирования значений
+    {
+        temp = start;
+        start = end;
+        end = temp;
+    }
+    if (start == end) return start;
+    return (start + SumNumbers(start + 1, end));
 }
 
 Console.WriteLine($"M = {M}; N = {N} -> {SumNumbers(M, N)}");
